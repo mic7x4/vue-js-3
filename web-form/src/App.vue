518049f8fde41a0d6a00c78583434052d6d35867
mic7x4 <mic7x4@gmail.com>
2022-09-27 00:37:16 +0200

    adding a single form input

---
+++ web-form/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <SignupForm/>
+</template>
+
+<script>
+  import SignupForm from './components/SignupForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SignupForm
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #333;
+  margin-top: 60px;
+}
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  background: #eee;
+}
+</style>
