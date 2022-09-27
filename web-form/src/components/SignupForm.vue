b40a24c6832dc2502256cf76d623a65f65ce2eae
mic7x4 <mic7x4@gmail.com>
2022-09-27 09:18:58 +0200

    deleting the skill from the dom

--- web-form/src/components/SignupForm.vue
+++ web-form/src/components/SignupForm.vue
@@ -13,7 +13,7 @@
     <label>Skills:</label>
     <input type="text" v-model="tempSkills" @keyup.alt="addSkill">
     <div v-for="skill in skills" :key="skill" class="pill">
-      {{skill}}
+      <span @click="deleteSkill(skill)">{{skill}}</span>
     </div>
 
     <div class="terms">
@@ -48,6 +48,11 @@ export default {
         }
         this.tempSkills = ''
       }
+    },
+    deleteSkill(skill) {
+      this.skills = this.skills.filter((item) => {
+        return skill !== item
+      })
     }
   }
 
@@ -88,5 +93,18 @@ export default {
       margin: 0 10px 0 0;
       position: relative;
       top: 2px;
+    }
+    .pill {
+      display: inline-block;
+      margin: 20px 10px 0 0 ;
+      padding: 6px 12px;
+      background: #eee;
+      border-radius: 20px;
+      font-size: 12px;
+      letter-spacing: 1px;
+      font-weight: bold;
+      color: #777;
+      cursor: pointer;
+
     }
 </style>
