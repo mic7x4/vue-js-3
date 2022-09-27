f4b759b64ca3ab4bd6cbbda70e5850951249856d
mic7x4 <mic7x4@gmail.com>
2022-09-28 01:25:26 +0200

    adding materials icons

---
+++ project-planner/src/components/SingleProject.vue
@@ -0,0 +1,59 @@
+<template>
+ <div class="project">
+    <div class="actions">
+        <h3 @click="show">{{project.title}}</h3>
+        <div class="icons">
+            <span class="material-icons">edit</span>
+            <span class="material-icons">delete</span>
+            <span class="material-icons">done</span>
+        </div>
+    </div>
+    <div v-if="showDetails" class="details">
+        <p>{{project.details}}</p>
+    </div>
+ </div>
+</template>
+
+<script>
+export default {
+    props: ['project'],
+    data () {
+        return {
+            showDetails:false
+        }
+    },
+    methods:{
+        show(){
+            return this.showDetails = !this.showDetails
+        }
+    }
+}
+</script>
+
+<style>
+.project {
+    margin: 20px auto;
+    background: white;
+    padding: 10px 20px;
+    border-radius: 4px;
+    box-shadow: 1px 2px 3px rgba(0,0,0,0.05);
+    border-left: 4px solid #e90074;
+}
+h3 {
+    cursor: pointer;
+}
+.actions {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+.material-icons {
+    font-size: 24px;
+    margin-left: 10px;
+    color: #bbb;
+    cursor: pointer;
+}
+.material-icons:hover {
+    color: #777;
+}
+</style>
